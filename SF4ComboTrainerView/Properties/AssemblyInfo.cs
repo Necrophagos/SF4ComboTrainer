c13e27bcf4d77a5a81195795783da72d7ab0584a
Tom Piddock <myturntohide@googlemail.com>
2014-01-15 17:43:47 +0000

    Added small copyright change to refer to the group, changed the default keyboard Inputs to be the game defaults and directly included InputSimulator.dll in the project content.

--- SF4ComboTrainerView/Properties/AssemblyInfo.cs
+++ SF4ComboTrainerView/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SF4ComboTrainer")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © SF4ComboTrainer Open Source Group 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
