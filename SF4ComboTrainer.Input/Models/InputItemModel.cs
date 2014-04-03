aae63f501b4e0fc6803075dc13775e037c8e7194
Tom Piddock <myturntohide@googlemail.com>
2014-04-03 02:39:02 +0100

    Input project controls the actual input for the game and the views and viewmodel associated with changing a combo's input item.

---
+++ SF4ComboTrainer.Input/Models/InputItemModel.cs
@@ -0,0 +1,256 @@
+﻿namespace SF4ComboTrainer.Input.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using SF4ComboTrainer.Input.Utilities;
+
+    /**
+    * the timeline classes are the core of this application.
+    * they represent an entry in the timeline listbox. there are currently 4 types
+    * press input, hold input, release input and wait frame.
+    * these differences are represented by the following class hierarchy
+    * 
+    * InputItemModel --- WaitFrameItemModel
+    *          |
+    *          |------ InputItemModel
+    *                      |
+    *                      |--- PressItemModel
+    *                      |--- HoldItemModel
+    *                      |--- ReleaseItemModel
+    */
+
+    /**
+     * The new layout will incorporate the InputItemModel as the base for Press, Hold and Release.
+     * However we will be removing WaitFrameItemModel and incorporating it into InputItemModel.
+     * There is no reason to have the WaitFrameItem as we can make the PressItemModel act upon
+     * its behalf by inputing a neutral input. This creates a waitframe without any inputs.
+     *      
+     *          InputItemModel
+     *               |
+     *               |--- PressItemModel
+     *               |--- HoldItemModel
+     *               |--- ReleaseItemModel
+     */
+
+    [Serializable()]
+    public class InputItemModel
+    {
+        public enum InputItemTypeEnum
+        {
+            Wait,
+            Press,
+            Hold,
+            Release
+        }
+
+        /// <summary>
+        /// The sound handler.
+        /// </summary>
+        protected static Roadie roadie = new Roadie();
+
+        /// <summary>
+        /// The type of the time line item.
+        /// </summary>
+        public InputItemTypeEnum InputItemType = InputItemTypeEnum.Wait;
+
+        /// <summary>
+        /// Array that defines directional inputs.
+        /// </summary>
+        public static Input[] Directions = new Input[] { Input.P1_UP, Input.P1_DN, Input.P1_LE, Input.P1_RI, Input.P1_BK, Input.P1_FW };
+
+        /// <summary>
+        /// Array that defines button inputs.
+        /// </summary>
+        public static Input[] Buttons = new Input[] { Input.P1_LP, Input.P1_MP, Input.P1_HP, Input.P1_LK, Input.P1_MK, Input.P1_HK };
+
+        /// <summary>
+        /// The frame duration of this Time Line Item.
+        /// </summary>
+        public int Frames { get; set; }
+
+        /// <summary>
+        /// If set to true, will play a sound on the time line item being played in the time line.
+        /// </summary>
+        public bool PlaySound = false;
+
+        public Input[] Inputs { get { return InputCommandState.ToInputsArray(); } }
+
+        /// <summary>
+        /// The InputCommandState that is configured by the view model.
+        /// </summary>
+        public InputCommandModel InputCommandState { get; set; }
+
+        /// <summary>
+        /// Abstract definition for serializing the inputs into a string. Must be defined by any inheritting classes
+        /// </summary>
+        /// <returns><see cref="string"/></returns>
+        public string Serialize()
+        {
+
+            String obj = Enum.GetName(typeof(InputItemTypeEnum), InputItemType) + "#" + PlaySound + "#" + Frames + "#" + Inputs.Count();
+            foreach (Input input in Inputs)
+            {
+                obj += "#" + input;
+            }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Static definition for deserializing a string into a set of inputs.
+        /// </summary>
+        /// <param name="value">The <see cref="string"/> value to deserialize.</param>
+        /// <returns><see cref="InputItemModel"/> containing the inputs from the string.</returns>
+        //public static InputItemModel Deserialize(String value)
+        //{
+        //    /**
+        //     * Creates a timeline object from a string serialization and assumes the following
+        //     * formats (specified by the respective Serialize methods)
+        //     * 
+        //     * InputItemModel:     ItemType#PlaySound#NumFrames#NumInputs[#Input1,#Input2...]
+        //     *                        
+        //     */
+        //    String[] tokens = value.ToUpper().Split('#');
+
+        //    // Big change: this is the new default to help transform old file type.
+        //    if (tokens[0].Equals(InputItemTypeEnum.WAIT.ToString()))
+        //    {
+        //        PressItemModel item = new PressItemModel(int.Parse(tokens[2]));
+        //        item.PlaySound = Boolean.Parse(tokens[1]);
+        //        return item;
+        //    }
+        //    else if (tokens[0].Equals(InputItemTypeEnum.PRESS.ToString()))
+        //    {
+        //        int numInputs = int.Parse(tokens[2]);
+        //        Input[] inputs = new Input[numInputs];
+        //        for (int i = 0; i < numInputs; i++)
+        //        {
+        //            inputs[i] = ParseInput(tokens[3 + i]);
+        //        }
+        //        PressItemModel item = new PressItemModel(inputs);
+        //        item.PlaySound = Boolean.Parse(tokens[1]);
+        //        return item;
+        //    }
+        //    else if (tokens[0].Equals(InputItemTypeEnum.HOLD.ToString()))
+        //    {
+        //        int numInputs = int.Parse(tokens[2]);
+        //        Input[] inputs = new Input[numInputs];
+        //        for (int i = 0; i < numInputs; i++)
+        //        {
+        //            inputs[i] = ParseInput(tokens[3 + i]);
+        //        }
+        //        HoldItemModel item = new HoldItemModel(inputs);
+        //        item.PlaySound = Boolean.Parse(tokens[1]);
+        //        return item;
+        //    }
+        //    else if (tokens[0].Equals(InputItemTypeEnum.RELEASE.ToString()))
+        //    {
+        //        int numInputs = int.Parse(tokens[2]);
+        //        Input[] inputs = new Input[numInputs];
+        //        for (int i = 0; i < numInputs; i++)
+        //        {
+        //            inputs[i] = ParseInput(tokens[3 + i]);
+        //        }
+        //        ReleaseItemModel item = new ReleaseItemModel(inputs);
+        //        item.PlaySound = Boolean.Parse(tokens[1]);
+        //        return item;
+        //    }
+
+        //    throw new FormatException("Failed to deserialize TimelineItem, wrong string format: " + value);
+        //}
+
+        public static InputItemModel Deserialize(string value) { 
+            return new InputItemModel( );
+        }
+
+        /// <summary>
+        /// Parses inputs from string to an input enum.
+        /// </summary>
+        /// <param name="str">The <see cref="string"/> to convert.</param>
+        /// <returns><see cref="Input"/></returns>
+        public static Input ParseInput(String str)
+        {
+            foreach (Input input in Directions)
+            {
+                if (str.Equals(input.ToString())) { return input; }
+            }
+            foreach (Input input in Buttons)
+            {
+                if (str.Equals(input.ToString())) { return input; }
+            }
+            throw new FormatException("Cannot parse Input for " + str);
+        }
+
+        /// <summary>
+        ///  Deconstructor to make the Roadie private.
+        /// </summary>
+        public static void DisposeRoadie()
+        {
+            roadie.Dispose();
+        }
+
+        public string GetInputString()
+        {
+            return GetInputString(Inputs);
+        }
+
+        public string GetInputString(params Input[] inputs)
+        {
+            String inputString = "";
+            foreach (Input input in inputs)
+            {
+                inputString += (input.ToString().Split('_'))[1] + " ";
+            }
+            return inputString;
+        }
+
+        /// <summary>
+        /// Action is called when timeline is played.
+        /// </summary>
+        /// <param name="sf4control">The SF4Control.</param>
+        /// <param name="sendInputs">If true, sends inputs to the running instance of SF4.</param>
+        public void Action(SF4Control sf4control, bool sendInputs)
+        {
+            if (sendInputs)
+            {
+                sf4control.Press(InputCommandState);
+            }
+            else
+            {
+                //press event waits 1 frame, if send inputs is disabled wait frame is maintained
+                sf4control.WaitFrames(1);
+            }
+
+            if (PlaySound)
+            {
+                if (Inputs.Intersect(Directions).Count() > 0 && Inputs.Intersect(Buttons).Count() > 0)
+                {
+                    roadie.PlaySound(Roadie.WAIT_SOUND);
+                }
+                else
+                {
+                    if (Inputs.Intersect(Directions).Count() > 0)
+                        roadie.PlaySound(Roadie.PRESS_DIRECTION_SOUND);
+                    if (Inputs.Intersect(Buttons).Count() > 0)
+                        roadie.PlaySound(Roadie.PRESS_BUTTON_SOUND);
+                }
+            }
+
+            //After sound has played we wait the remainder of the frame duration
+            sf4control.WaitFrames(Frames - 1);
+        }
+
+        public InputItemModel(InputCommandModel inputCommand)
+        {
+            InputCommandState = inputCommand;
+            InputItemType = InputItemTypeEnum.Wait;
+        }
+        
+        public InputItemModel() : this(new InputCommandModel()){}
+    }
+}
---
+++ SF4ComboTrainer.Input/Utilities/SF4Control.cs
@@ -0,0 +1,288 @@
+﻿namespace SF4ComboTrainer.Input.Utilities
+{
+
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using WindowsInput;
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+    using System.Threading;
+
+    using SF4ComboTrainer.Input;
+    using SF4ComboTrainer.Input.Models;
+    using SF4ComboTrainer.Input.Utilities.MemoryEditor;
+
+    /**
+     * this class is the is the interface to streetfighter. it used the sf4memory to read data from sf4 and it uses the 
+     * inputsimulator to send keyboad strokes.
+     */
+    public class SF4Control
+    {
+
+        #region API calls
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+        #endregion
+
+        #region Fields
+        protected SF4Memory sf4memory;
+
+        public InputResolver inputResolver;
+
+        // To keep the last frame.
+        protected int lastFrame;
+
+        // To tell if the game is paused.
+        protected Stopwatch frameTimer = new Stopwatch();
+
+        //min time in between frames is set to 3 seconds just so to prevent lockups when game is actually paused
+        //switch to fullscreen sf4 actually takes very long.
+        protected int MIN_TIME_BETWEEN_FRAMES = 3000;
+
+        // to see if in a match.
+        public volatile bool InMatch;
+        #endregion
+
+        public bool SwitchToSF4()
+        {
+            Process[] processes = Process.GetProcessesByName("SSFIV");
+            if (processes.Count() > 0)
+            {
+                SetForegroundWindow(processes[0].MainWindowHandle);
+                ShowWindow(processes[0].MainWindowHandle, 1);
+                return true;
+            }
+            return false;
+        }
+
+        public bool SF4isForegroundWindow()
+        {
+            Process[] processes = Process.GetProcessesByName("SSFIV");
+            if (processes.Count() > 0)
+            {
+                return processes[0].MainWindowHandle == GetForegroundWindow();
+            }
+            return false;
+        }
+
+        public SF4Control(SF4Memory sf4memory)
+        {
+            this.sf4memory = sf4memory;
+            this.inputResolver = new InputResolver(sf4memory);
+        }
+
+        public void WaitFrames(int frames)
+        {
+            int currentFrame = sf4memory.GetFrameCount();
+            int endFrame = currentFrame + frames;
+            long lastFrameTime = frameTimer.ElapsedMilliseconds;
+            
+            while (currentFrame < endFrame 
+                && (frameTimer.ElapsedMilliseconds - lastFrameTime) < MIN_TIME_BETWEEN_FRAMES)
+            {
+                // Set lastFrame then the new current frame
+                lastFrame = currentFrame;
+                currentFrame = sf4memory.GetFrameCount();
+
+                if (currentFrame != lastFrame)
+                {
+                    lastFrameTime = frameTimer.ElapsedMilliseconds;
+
+                    // Since we currentFrame != lastFrame we are in a match.
+                    // (frames on menu screen or pause menu are constant).
+                    InMatch = true;
+                }
+                else
+                {
+                    InMatch = false;
+                    break;
+                }
+
+                Thread.Sleep(1);
+            }
+            
+        }
+
+        public void ResetLockupTimer()
+        {
+            if (frameTimer.IsRunning)
+            {
+                frameTimer.Stop();
+            }
+            frameTimer.Reset();
+            frameTimer.Start();
+        }
+
+        public void Press(InputCommandModel inputCommandModel)
+        {
+            if (!SF4isForegroundWindow()) { return; }
+
+            foreach (Input input in inputCommandModel.ToInputsArray())
+            {
+                InputSimulator.SimulateKeyDown(inputResolver.Get(input));
+            }
+            WaitFrames(1);
+            foreach (Input input in inputCommandModel.ToInputsArray())
+            {
+                InputSimulator.SimulateKeyUp(inputResolver.Get(input));
+            }
+
+            //TODO: To unify a press and the wait that follows - Add a WaitFrames(framesToWait - 1);
+
+        }
+
+        public void Hold(InputCommandModel inputCommandModel)
+        {
+            if (!SF4isForegroundWindow()) { return; }
+
+            foreach (Input input in inputCommandModel.ToInputsArray())
+            {
+                InputSimulator.SimulateKeyDown(inputResolver.Get(input));
+            }
+        }
+
+        public void Release(InputCommandModel inputCommandModel)
+        {
+            if (!SF4isForegroundWindow()) { return; }
+
+            foreach (Input input in inputCommandModel.ToInputsArray())
+            {
+                InputSimulator.SimulateKeyUp(inputResolver.Get(input));
+            }
+        }
+
+        public void ReleaseALL()
+        {
+            foreach (VirtualKeyCode key in inputResolver.InputMap.Values)
+            {
+                InputSimulator.SimulateKeyUp(key);
+            }
+        }
+
+    //    #region Recording System
+
+    //   // private SF4InputHandler inputHandler;
+    //    //private List<SF4InputState> inputsList;
+
+    //    //Recording thread section
+    //    private System.Threading.Thread recordThread = null;
+    //    private volatile bool recordingActive;
+
+    //    public delegate void RecordedInputEvent(InputItemModel timeLineItem);
+    //    public event RecordedInputEvent OnRecordInput;
+    //    public delegate void ResetInputEvent();
+    //    public event ResetInputEvent OnResetInput;
+
+    //    private void recordForFrames(object maxFrames)
+    //    {
+
+    //        int currentFrame = sf4memory.GetFrameCount();
+    //        int endFrame = currentFrame + (int)maxFrames;
+
+    //        int waitGap = 0;
+
+    //        //If the state stays empty - add in waittimelineitem
+    //        // SF4InputState prevState = null;
+    //        List<InputItemModel> timeLineItems = new List<InputItemModel>();
+
+    //        // Reset / start the frameTimer which is used to get time between frames.
+    //        frameTimer.Reset();
+    //        frameTimer.Start();
+
+    //        while (currentFrame < endFrame && frameTimer.ElapsedMilliseconds < MIN_TIME_BETWEEN_FRAMES && recordingActive)
+    //        {
+    //            // Set lastFrame then the new current frame
+    //            lastFrame = currentFrame;
+    //            currentFrame = sf4memory.GetFrameCount();
+
+    //            if (currentFrame != lastFrame)
+    //            {
+    //                // Stop the frame timer since the frame has changed.
+    //                frameTimer.Stop();
+
+    //                //Time to check inputs
+    //                inputHandler.InputUpdate(); //Get controllers input state this frame
+
+    //                //Use the back button on the controller to reset the timeline
+    //                if (inputHandler.CurrentState.Back)
+    //                    OnResetInput();
+
+
+    //                //If no input - increment the wait gap so we can get timings
+    //                if (inputHandler.CurrentState.NonePressed == false)
+    //                {
+    //                    //If nothing pressed in last frame but something pressed now -
+    //                    // add the wait time to the list and 
+    //                    if (prevState.NonePressed)
+    //                    {
+    //                        OnRecordInput(new PressItemModel(waitGap));
+    //                        waitGap = 0;
+    //                    }
+    //                    else if (inputHandler.CurrentState != prevState)
+    //                    {
+    //                        OnRecordInput(new PressItemModel(inputHandler.CurrentState.ToInputsArray()));
+    //                        Debug.WriteLine("RECORDED INPUT");
+    //                    }
+    //                }
+    //                else
+    //                {
+    //                    waitGap++;
+    //                }
+
+    //                prevState = inputHandler.CurrentState;
+
+    //                frameTimer.Reset();
+    //                frameTimer.Start();
+
+    //                // Since we currentFrame != lastFrame we are in a match.
+    //                // (frames on menu screen or pause menu are constant).
+    //                InMatch = true;
+    //            }
+    //            else
+    //                InMatch = false;
+
+    //            Thread.Sleep(1);
+    //        }
+    //    }
+
+    //    public void ConnectToController(int playerSlot)
+    //    {
+    //        //inputHandler = new SF4InputHandler(1, SF4InputHandler.InputType.XBoxController);
+    //    }
+    //    public void Record()
+    //    {
+    //        //inputHandler.InputUpdate();
+    //        //SF4InputState state = inputHandler.CurrentState;
+    //    }
+
+
+    //    public void StartRecording()
+    //    {
+
+    //        //prevent multiple threads
+    //        if (null != recordThread) { return; }
+    //        recordingActive = true;
+    //        recordThread = new System.Threading.Thread(recordForFrames);
+    //        recordThread.Start(100000);
+    //    }
+
+    //    public void StopRecording()
+    //    {
+    //        recordingActive = false;
+    //    }
+
+ 
+    //
+    //    #endregion
+
+    }
+}
