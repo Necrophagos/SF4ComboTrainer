fb1db5f4eceda5ff2a4bac4beb33ddbc9381d2ce
Tom Piddock <myturntohide@googlemail.com>
2014-04-03 02:40:09 +0100

    The timeline controls the flow from one input item to the next and works on displaying the combos to the user.

---
+++ SF4ComboTrainer.Timeline/Views/TimeLineItemView.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace SF4ComboTrainer.TimeLine.Views 
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    using Xceed.Wpf.Toolkit;
+
+    using SF4ComboTrainer.Common.Converters;
+
+    /// <summary>
+    /// Interaction logic for InputItemView.xaml
+    /// </summary>
+    public partial class TimeLineItemView : StackPanel
+    {
+        public TimeLineItemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
