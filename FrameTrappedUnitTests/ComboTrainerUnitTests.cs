49ecb352dd4cac37fee198590a811bae0f81a79f
Tom Piddock <tpiddock@gmail.com>
2014-05-13 02:27:12 +0100

    Unit test for process creation.

---
+++ FrameTrappedUnitTests/ComboTrainerUnitTests.cs
@@ -0,0 +1,40 @@
+﻿namespace FrameTrappedUnitTests
+{
+    using System;
+    using System.Diagnostics;
+    using FrameTrapped.ComboTrainer.Utilities;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ComboTrainerUnitTests
+    {
+        [TestMethod]
+        public void RunStreetFighterIVAE()
+        {
+            Process PR = SF4ProcessHandler.Instance.GetSF4();
+
+            if (PR.ProcessName != "SSFIV")
+            {
+                Assert.Fail("The SF4ProcessHandler failed to create a SF4 process.");
+            }
+        }
+
+        [TestMethod]
+        public void CloseStreetFighterIVAE()
+        {
+            Process PR = SF4ProcessHandler.Instance.GetSF4();
+            
+            if (PR.ProcessName != "SSFIV")
+            {
+                Assert.Fail("The SF4ProcessHandler failed to create a SF4 process.");
+            }
+
+            SF4ProcessHandler.Instance.StopSF4();
+
+            if (!PR.HasExited)
+            {
+                Assert.Fail("The SF4ProcessHandler failed to close the SF4 process.");
+            }
+        }
+    }
+}
