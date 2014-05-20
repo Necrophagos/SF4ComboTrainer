c05690c9fce1941229311ed8811a3f95b226a80a
Tom Piddock <tpiddock@gmail.com>
2014-05-20 17:36:47 +0100

    Issue 10 - Make time line items resolve to the correct item in the list after removing or adding a timeline item

--- FrameTrapped.ComboTrainer/ViewModels/TimeLineViewModel.cs
+++ FrameTrapped.ComboTrainer/ViewModels/TimeLineViewModel.cs
@@ -28,7 +28,7 @@
         private bool _autoSwitchToSF4;
 
         private bool _isSteamVersion;
-        
+
         private TimeLineItemViewModel _selectedTimeLineItem;
 
         public TimeLineItemViewModel SelectedTimeLineItem
@@ -74,34 +74,30 @@
                 }
             }
         }
-         
+
         public bool CanRemoveItem
         {
-            get { return TimeLineItems.Count > 0; }
+            get { return TimeLineItems.Count > 1; }
         }
-          
+
         public void AddTimeLineItem()
         {
             TimeLineItemViewModel newTimeLineItemViewModel = new TimeLineItemViewModel(this);
+            AddTimeLineItem(newTimeLineItemViewModel);
 
-            TimeLineItems.Add(newTimeLineItemViewModel);
-
-            SelectedTimeLineItem = newTimeLineItemViewModel;           
-
-            NotifyOfPropertyChange(() => CanRemoveItem);
-            NotifyOfPropertyChange(() => TimeLineItems);
         }
 
         public void AddTimeLineItem(TimeLineItemViewModel timeLineItemViewModel)
         {
-            TimeLineItems.Add(timeLineItemViewModel);
+            TimeLineItems.Insert(TimeLineItems.IndexOf(SelectedTimeLineItem) + 1, timeLineItemViewModel);
 
             SelectedTimeLineItem = timeLineItemViewModel;
+
 
             NotifyOfPropertyChange(() => CanRemoveItem);
             NotifyOfPropertyChange(() => TimeLineItems);
         }
-    
+
         public void AppendTimeLine()
         {
             OpenTimeLine(true);
@@ -111,7 +107,7 @@
         {
             if (CanRemoveItem)
             {
-                TimeLineItems.RemoveAt(TimeLineItems.Count - 1);
+                TimeLineItems.Remove(SelectedTimeLineItem);
 
                 SelectedTimeLineItem = TimeLineItems.Last();
 
@@ -138,7 +134,7 @@
                 _events.Publish(new PlayTimeLineMessage(TimeLineItems));
             }
         }
-         
+
         public void OpenTimeLine(bool append = false)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
@@ -160,7 +156,7 @@
                 }
             }
         }
- 
+
         public void SaveTimeLine()
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
@@ -202,8 +198,9 @@
             _events.Subscribe(this);
 
             TimeLineItems = new BindableCollection<TimeLineItemViewModel>();
+            TimeLineItems.Add(new TimeLineItemViewModel(this));
 
-            AddTimeLineItem();
+            SelectedTimeLineItem = TimeLineItems.Last();
 
             _autoSwitchToSF4 = true;
             _isSteamVersion = true;
